8a2caed1e5ae37484a5f0c91d2121ad139591d0a
PWHsueh <pwhsueh@gmail.com>
2015-01-19 08:17:18 +0800

    no message

--- fuel/modules/cpinput/views/_admin/cpinput_create_view.php
+++ fuel/modules/cpinput/views/_admin/cpinput_create_view.php
@@ -59,7 +59,7 @@
 						</div>	
 						<div class="form-group">
 							<div class="col-sm-12" style="text-align:center">
-								<button type="submit" class="btn btn-info">新增</button>
+								<button type="submit" class="btn btn-info">儲存</button>
 								<button type="button" class="btn btn-danger" onClick="aHover('<?php echo $module_uri?>')">取消</button>
 							</div>
 						</div>
